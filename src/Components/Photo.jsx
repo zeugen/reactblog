f2c527d2dbbd51da723169b58ab863d265d5406d
Zeugen <zeugen@users.noreply.github.com>
2021-09-22 03:55:40 +0300

    added a way to update state in the main component

--- src/Components/Photo.jsx
+++ src/Components/Photo.jsx
@@ -9,7 +9,14 @@ function Photo(props) {
         <p>{post.description}</p>
       </figcaption>
       <div className="button-container">
-        <button className="remove-button">Remove</button>
+        <button
+          className="remove-button"
+          onClick={() => {
+            props.onRemovePhoto(post);
+          }}
+        >
+          Remove
+        </button>
       </div>
     </figure>
   );
--- src/Components/Photowall.jsx
+++ src/Components/Photowall.jsx
@@ -2,30 +2,16 @@ import React from "react";
 import Photo from "./Photo";
 
 function Photowall(props) {
+  console.log(props);
   const postsData = props.posts;
-  console.log(postsData);
 
   return (
     <div className="photoGrid">
       {postsData.map((post, index) => (
-        <Photo key={index} post={post} />
+        <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />
       ))}
     </div>
   );
 }
-// class Photowall extends Component {
-//   render(props) {
-//     const postsData = this.props.posts;
-//     console.log(postsData);
-
-//     return (
-//       <div className="photoGrid">
-//         {postsData.map((post, index) => (
-//           <Photo key={index} post={post} />
-//         ))}
-//       </div>
-//     );
-//   }
-// }
 
 export default Photowall;
